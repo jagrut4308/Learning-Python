eef799bca9af7c4e103a76f839a1731590a307fa
jagrut4308 <jagruthanand@gmail.com>
2026-09-16 12:37:30 +0000

    changes

--- learninglists.py
+++ learninglists.py
@@ -1,5 +1,17 @@
-directors=["Kuroswa","Ray","Tarantino"]
+directors=["Kuroswa","Ray","Tarantino","Kashyap","Bergman","Joon-Ho"]
 print(directors)
 print(directors[2])
 print(directors[-1])
 #negative index starts indexing from back of the list
+print(directors[1:])
+#all elements from index 1
+print(directors[1:3])
+#elements from index 1 to index 3,not including 3
+directors[1]="Satyajit Ray"
+print(directors)
+#LIST FUNCTIONS
+ndirect=["Nolan","Scorsese"]
+directors.extend(ndirect)
+print(directors)
+directors.append("Coen")
+print(directors)
