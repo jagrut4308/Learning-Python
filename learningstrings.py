a28d879fc4d5a6f9eda6d60a6e440fe0814fd124
jagrut4308 <jagruthanand@gmail.com>
2026-09-16 06:29:08 +0000

    changes

--- learningstrings.py
+++ learningstrings.py
@@ -6,3 +6,10 @@ phrase1="JAGRUT"
 print(phrase1.isupper())
 print(len(phrase))
 print(phrase[0])
+print(phrase.index('g'))
+print(phrase.replace("Jagrut","Devadiga"))
+#original string not changed
+print(phrase)
+#to change original string
+phrase=phrase.replace("Ja","Ra")
+print(phrase)
