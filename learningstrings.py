764648cb99ebee88ed24b4979008aad11dfcf066
jagrut4308 <jagruthanand@gmail.com>
2026-09-16 06:23:41 +0000

    updated

--- learningstrings.py
+++ learningstrings.py
@@ -1,2 +1,8 @@
 print("Things are \"weird\"")
 #\"" allows us to print double quotes
+phrase="Jagrut"
+print(phrase.isupper())
+phrase1="JAGRUT"
+print(phrase1.isupper())
+print(len(phrase))
+print(phrase[0])
