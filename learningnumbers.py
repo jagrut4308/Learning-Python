0227518ca5b43c0dc8541dad73aea09391c37c89
jagrut4308 <jagruthanand@gmail.com>
2026-09-16 06:38:01 +0000

    changes

---
+++ learningnumbers.py
@@ -0,0 +1,16 @@
+from math import *
+#imports everything from math
+num=123.43
+num=str(num)
+print(num[0])
+#string operations can be performed on number after converting it to string
+num=-6
+print(abs(num))
+print(pow(5,4))
+print(max(5,1))
+print(min(-1,3))
+print(round(3.2))
+print(round(3.5))
+#math needs to be imported for ceil,floor
+print(ceil(3.1))
+print(floor(3.9))
