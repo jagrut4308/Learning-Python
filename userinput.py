13e6619b4deddbe6972b694c801a429a486978f3
jagrut4308 <jagruthanand@gmail.com>
2026-09-16 06:50:31 +0000

    add

--- userinput.py
+++ userinput.py
@@ -1,3 +1,13 @@
 age=input("Enter your age:")
 name=input("Enter your name:")
 print("Hi",name,",you are",age,"years old.")
+print("Simple calculator")
+num1=input("Enter num1:")
+num2=input("Enter num2:")
+sum=num1+num2
+print(sum)
+#input must be converted into int
+num1=int(input("Enter num1:"))
+num2=int(input("Enter num2:"))
+sum=num1+num2
+print(sum)
