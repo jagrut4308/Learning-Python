b6b65fbb91199ef7ddc78448749d815ff5099fe4
jagrut4308 <jagruthanand@gmail.com>
2026-09-17 16:23:41 +0000

    add

--- simple loop programs/password.py
+++ simple loop programs/password.py
@@ -1,1 +1,8 @@
-
+password="Jagrut"
+nguess=0
+guess=""
+while(guess!=password and nguess!=3):
+    guess=input("Enter password:")
+    nguess+=1
+if(guess==password):
+    print("Correct password")
