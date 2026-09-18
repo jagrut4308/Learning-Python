160fba3a69314462477fc194db5e6e72ec5e05f8
jagrut4308 <jagruthanand@gmail.com>
2026-09-18 05:34:24 +0000

    add

--- learningfiles.py
+++ learningfiles.py
@@ -4,3 +4,11 @@ print(s.readline())
 for i in s.readlines():
     print(i)
 s.close()
+o=open("practice.txt","a")
+o.write("\nPractice makes a man perfect")
+o.close()
+a=int(input("Enter a number:"))
+if a==1:
+    o=open("practice1.text","w")#creates new file practice1
+    o.write("Overwrite")
+    o.close()
