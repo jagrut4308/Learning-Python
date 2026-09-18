800335c987888bdf325088598907a074330dc187
jagrut4308 <jagruthanand@gmail.com>
2026-09-18 06:15:18 +0000

    add

--- learningmodules.py
+++ learningmodules.py
@@ -1,3 +1,7 @@
 import practicemodule
 print(practicemodule.expo(6,2))
 #imports practicemodule.py and uses def expo in that
+#check preexisting python modules
+#to install 3rd party modules,use pip
+#pip install python-docx (in this case while installing python-docx)
+#use the above cmd in command module or terminal
