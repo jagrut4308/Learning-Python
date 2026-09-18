20e91061ba7a9b1aa896acf096c41b289ab763a9
jagrut4308 <jagruthanand@gmail.com>
2026-09-18 06:26:14 +0000

    add

---
+++ learningclasses.py
@@ -0,0 +1,7 @@
+class student:#class helps create our own customised data type
+    def __init__(self,name,major,cgpa):
+    #def _init_(self) gives characteristics to our data type
+    #it defines what a student is in this case
+        self.name=name
+        self.major=major
+        self.cgpa=cgpa
---
+++ learningclasses1.py
@@ -0,0 +1,3 @@
+from learningclasses import student
+#imports student class from learning classes file
+student1=student("Jagrut","CSE",9.3)
