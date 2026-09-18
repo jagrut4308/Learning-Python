67ce48db6d16c8b997143fee1d3d6884a90a1ad2
jagrut4308 <jagruthanand@gmail.com>
2026-09-18 06:06:35 +0000

    add

---
+++ learningmodules.py
@@ -0,0 +1,3 @@
+import practicemodule
+print(practicemodule.expo(6,2))
+#imports practicemodule.py and uses def expo in that
---
+++ practicemodule.py
@@ -0,0 +1,5 @@
+def expo(n,p):
+    res=1
+    for i in range(p):
+        res=res*n
+    return res;
